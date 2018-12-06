fbd712efabbbe6ebaa31204a4d9c8538bd7f1cbc
Arcaedion <45541068+Arcaedion@users.noreply.github.com>
2018-12-06 16:56:59 +0000

    Add files via upload

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Transform objetoAlvo;
+	
+	void Update () {
+        transform.position = objetoAlvo.position;	
+	}
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float Velocidade;
+    public float ImpulsoPulo;
+    public KeyCode ComandoDePulo;
+
+    private bool _estaNoChao;
+
+    void Update()
+    {
+        if (Input.GetKey(ComandoDePulo) && _estaNoChao)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, ImpulsoPulo, 0), ForceMode.Impulse);
+            _estaNoChao = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        _estaNoChao = true;
+    }
+
+    void FixedUpdate()
+    {
+        var corpo = GetComponent<Rigidbody>();
+        corpo.velocity = new Vector3(Velocidade, corpo.velocity.y, corpo.velocity.z);
+    }
+}
